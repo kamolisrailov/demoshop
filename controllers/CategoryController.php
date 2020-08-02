b13a7a879f95cb4863882c531097b570b7d6175b
kamolisrailov <mr.lengdon99@gmail.com>
2020-08-02 12:35:33 +0500

    search added

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -31,4 +31,18 @@ class CategoryController extends AppController
 
     }
 
+    public function actionSearch()
+    {
+        $q = \Yii::$app->request->get('q');
+        $this->setMeta(" Поиск:  {$q} :: " . \Yii::$app->name);
+        if(!$q){
+            return $this->render('search');
+        }
+        $query = Product::find()->where(['like', 'title', $q]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 4, 'pageSizeParam' => false, 'forcePageParam' => false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
+        return $this->render('search', compact('products', 'pages', 'q'));
+    }
+
 }
