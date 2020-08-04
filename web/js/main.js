f28fcb78e7cf18cc4903bd09325f2cf73a6d3098
kamolisrailov <mr.lengdon99@gmail.com>
2020-08-04 14:01:43 +0500

    checkout added

--- web/js/main.js
+++ web/js/main.js
@@ -41,6 +41,15 @@ jQuery(document).ready(function($) {
     $().UItoTop({ easingType: 'easeOutQuart' });
 
 
+    $(function(){
+        $('#example').okzoom({
+            width: 150,
+            height: 150,
+            border: "1px solid black",
+            shadow: "0 0 5px #000"
+        });
+    });
+
 });
 
 $(window).load(function(){
@@ -73,3 +82,85 @@ paypal.minicart.cart.on('checkout', function (evt) {
         evt.preventDefault();
     }
 });
+
+
+
+
+/* Cart */
+
+function showCart(cart){
+    $('#modal-cart .modal-body').html(cart);
+    $('#modal-cart').modal();
+    let cartSum = $('#cart-sum').text() ? $('#cart-sum').text() : '$0';
+    if (cartSum){
+        $('.cart-sum').text(cartSum);
+    }
+}
+
+function getCart(){
+    $.ajax({
+        url: 'cart/show',
+        type: 'GET',
+        success: function (res) {
+            if (!res){ alert('Ошибка');}
+            showCart(res);
+        },
+        error: function () {
+            alert('Error');
+        }
+    });
+}
+
+function clearCart(){
+    $.ajax({
+        url: 'cart/clear-cart',
+        type: 'GET',
+        success: function (res) {
+            if (!res){ alert('Ошибка');}
+            showCart(res);
+        },
+        error: function () {
+            alert('Error');
+        }
+    });
+}
+
+
+
+    $('.add-to-cart').on('click', function () {
+
+        let id = $(this).data('id');
+        $.ajax({
+            url: 'cart/add',
+            data: {id: id},
+            type: 'GET',
+            success: function (res) {
+                if (!res){ alert('Ошибка');}
+                showCart(res);
+            },
+            error: function () {
+                alert('Error');
+            }
+        });
+        return false;
+    });
+
+$('#modal-cart .modal-body').on('click', '.del-item', function () {
+
+    let id = $(this).data('id');
+    $.ajax({
+        url: 'cart/del-item',
+        data: {id: id},
+        type: 'GET',
+        success: function (res) {
+            if (!res){ alert('Ошибка');}
+            showCart(res);
+        },
+        error: function () {
+            alert('Error');
+        }
+    });
+})
+
+
+/* Cart */
