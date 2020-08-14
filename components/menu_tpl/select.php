50e7e23e8cc0d72aa96252c52eccebdb4cab5739
kamolisrailov <mr.lengdon99@gmail.com>
2020-08-14 21:53:14 +0500

    category manage added

--- components/menu_tpl/select.php
+++ components/menu_tpl/select.php
@@ -1,1 +1,18 @@
-<?php
+<option value="<?=
+/* @var $category */
+/* @var $tab */
+$category['id'] ?>"
+
+    <?php if($category['id'] == $this->model->parent_id) echo 'selected' ?>
+    <?php if($category['id'] == $this->model->id) echo 'disabled' ?>
+>
+
+    <?= " {$tab} {$category['title']} " ?>
+
+
+
+</option>
+
+<?php if(isset($category['children'])):   ?>
+     <?= $this->getMenuHtml($category['children'], $tab . '-') ?>
+<?php endif;  ?>
